ee200be2ad5b2137c5db92c9a05c860d76895e2d
박태수 <taesupark0504@gmail.com>
2025-07-06 23:22:02 +0900

    Add other project files

---
+++ src/test/java/com/example/fcm_practice/FcmPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcm_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
